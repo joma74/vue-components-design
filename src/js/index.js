919146ce30522775b67e4c6be66fa5165e5f69ae
joma74 <dev-mails@gmx.net>
2018-09-30 23:32:50 +0200

    chore(setup): husky with lint

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 // 1:5  warning  'a' is assigned a value but never used        no-unused-vars
 // 1:5  error    'a' is never reassigned. Use 'const' instead  prefer-const
-let a = true
+let ab = true
